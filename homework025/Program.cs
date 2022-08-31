dab230e184e245874858aa8f2a24c80c65e0715f
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-01 00:43:13 +0600

    возведение числа a в степень b, при вводе их с клавиатуры

---
+++ homework025/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int stepen = 1;
+for( int i = 1; i<= b; i++ )
+{
+    stepen = stepen *a;
+}
+Console.WriteLine();
+Console.WriteLine(a + "^" + b + " = " + stepen);
