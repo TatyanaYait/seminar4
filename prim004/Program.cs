a286207958fce76b91ec329118733eb3f35df91e
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-01 00:04:02 +0600

    формирование массива из 8 элементов, состоящего из 0 и 1

---
+++ prim004/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] numbA)
+{
+    int length = numbA.Length;
+    int index = 0;
+    while (index < length)
+    {
+        numbA[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] numbB)
+{
+    int count = numbB.Length;
+    int posit = 0;
+    while (posit < count)
+    {
+        Console.Write(numbB[posit]);
+        posit++;
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine();
