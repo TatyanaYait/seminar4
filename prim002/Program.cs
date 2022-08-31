daeab87f4b162e69f2300661de788f42d2dbd28c
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-31 23:08:47 +0600

    вывод количества цифр в введенном числе

---
+++ prim002/Program.cs
@@ -0,0 +1,16 @@
+﻿int CountNumber(int n)
+{
+    int res = 0;
+    n = Math.Abs(n);   ///взятие числа по модулю
+    while ( n >= 0)
+    {
+        res ++;
+        n = n/10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int itog = CountNumber(a);
+Console.WriteLine(itog);
