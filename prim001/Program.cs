5e9c3255212877f5f607b5bfd03e8a9ac94fba85
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-31 22:46:49 +0600

    функция вывода суммы от 1 до а

---
+++ prim001/Program.cs
@@ -0,0 +1,26 @@
+﻿/*void SumNumbers(int n) {  // функция, позволяющая работать толлько с консолью
+    int sum = 0;
+    for (int i = 1; i <= n; i++)    {
+        sum = sum + i;  //  sum += i;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+SumNumbers(a);   // вызов функции с аргументом, введенным с клавиатуры*/
+
+int SumNumbers(int n) {  
+    int sum = 0;
+    for (int i = 1; i <= n; i++)    {
+        sum = sum + i;  //  sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+SumNumbers(a);   // int result = SumNumbers(a);
+Console.WriteLine(SumNumbers(a)); // или задать переменную, в которую записать результат 
