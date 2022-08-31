8679558708c9b927dfbb9b29ec25ecca54605b10
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-01 02:20:24 +0600

    сумма цифр числа, введенного с клавиатуры

---
+++ homework027/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int ElementSum = 0;
+
+if (a / 10 > 0)
+{
+    while (a / 10 >= 1)
+    {
+        int n1 = a % 10;
+        ElementSum = ElementSum + n1;
+        a = a / 10;
+    }
+}
+int result = ElementSum + a % 10;
+Console.WriteLine("Сумма цифр, составляющих число равна " + result);
+
+
