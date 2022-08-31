10feb383cc3c97da9a75097cc1f95000665c3391
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-31 23:51:51 +0600

    нахождение факториала числа

---
+++ prim003/Program.cs
@@ -0,0 +1,20 @@
+﻿int Factorial(int a)
+{
+    int f = 1;
+    for(int i = 1; i <= a; i++)
+    {
+         f = f * i;
+        }
+   return f;
+}
+
+
+Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+ while (b <= 1 )
+    {
+        Console.WriteLine("Введите число больше единицы ");
+        b = int.Parse(Console.ReadLine());
+    }
+Factorial(b);
+Console.WriteLine("факториал числа равен " + Factorial(b));
