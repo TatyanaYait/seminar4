ab4255920d7e8dd5accd277468728cb7eb559600
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-02 14:54:59 +0600

    добавлен коментарий относительно размера массива

--- homework029/Program.cs
+++ homework029/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.Write("Введите желаемый размер массива: ");
-int n = int.Parse(Console.ReadLine());
+﻿//Console.Write("Введите желаемый размер массива: ");
+//int n = int.Parse(Console.ReadLine());
 
 Console.Write("Введите минимальное значение элемента: ");
 int min = int.Parse(Console.ReadLine());
@@ -7,7 +7,7 @@ int min = int.Parse(Console.ReadLine());
 Console.Write("Введите максимальное значение элемента: ");
 int max = int.Parse(Console.ReadLine());
 
-int[] array = new int [n];
+int[] array = new int [8];    //new int[n] если размер не задан, то определяем его из первого запроса на ввод
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(min, max+1);
