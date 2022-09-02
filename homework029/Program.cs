dcc129327412724bffc6cc9a7913019c6f809b9d
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-02 14:49:14 +0600

    генерация произвольного массива и вывод его в консоль

---
+++ homework029/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите желаемый размер массива: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Введите минимальное значение элемента: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Введите максимальное значение элемента: ");
+int max = int.Parse(Console.ReadLine());
+
+int[] array = new int [n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(min, max+1);
+}
+Console.WriteLine();
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine();
+
+ 
